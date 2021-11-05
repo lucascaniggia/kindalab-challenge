089f5e44dc15de72c57a553c57a48899d53208bf
Lucas Caniggia <lucascaniggia5@gmail.com>
2021-11-05 20:42:02 -0300

    Project development. Base app working, need feature adaptations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cocktails: [],
+    };
+  }
+  changeHandler = (e) => {
+    const letter = e.target.value;
+    let url =
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=` + letter;
+    if (letter === '') {
+      url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ cocktails: data }));
+  };
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
+  result = () => {
+    const data = this.state.cocktails.drinks;
+    if (data) {
+      return data.map((cocktail) => (
+        <div className="main-card" key={cocktail.idDrink}>
+          <img
+            src={cocktail.strDrinkThumb}
+            alt="cocktails"
+            height="200"
+            width="200"
+          />
+          <br />
+          <h2 className="main-title">{cocktail.strDrink}</h2>
+          <p className="main-ing">
+            Ingredients:
+            {cocktail.strIngredient1}, {cocktail.strIngredient2},{' '}
+            {cocktail.strIngredient3}
+          </p>
+        </div>
+      ));
+    }
+  };
+  render() {
+    return (
+      <>
+        <div className="main-container">
+          <form onSubmit={this.submitHandler}>
+            <div className="search-container">
+              <input
+                type="search"
+                className="search-field"
+                onChange={this.changeHandler}
+              />
+              <br />
+              <button className="submit-btn">SEARCH COCKTAILS</button>
+            </div>
+          </form>
+        </div>
+        {this.result()}
+      </>
+    );
+  }
 }
 
 export default App;
